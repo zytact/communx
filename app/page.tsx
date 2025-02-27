3acf74ee5fd540f8f16db54d5ca1f66672383f12
Arnab Chakraborty <iam-arnab@outlook.com>
2025-02-27 18:28:59 +0530

    Add WavyBackground component and replace ModeToggle with TypewriterEffect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+'use client';
 import { FloatingNav } from '@/components/ui/floating-navbar';
-import { ModeToggle } from '@/components/ui/mode-toggle';
+import { TypewriterEffect } from '@/components/ui/TypeWriter';
+import { WavyBackground } from '@/components/ui/wavy-background';
 import { Home as HomeIcon, CircleHelp } from 'lucide-react';
 
 export default function Home() {
@@ -15,12 +17,26 @@ export default function Home() {
             icon: <CircleHelp />,
         },
     ];
+
     return (
         <main className="relative h-screen w-screen overflow-hidden">
-            <div className="absolute top-4 right-4 z-10 flex flex-col gap-2">
-                <ModeToggle />
-            </div>
+            <div className="absolute top-4 right-4 z-10 flex flex-col gap-2"></div>
             <FloatingNav navItems={navItems} />
+            <WavyBackground
+                backgroundFill="#171717"
+                colors={['#a61717', '#e53e17', '#ba3e17']}
+            >
+                <TypewriterEffect
+                    words={[
+                        { text: 'The' },
+                        { text: 'best' },
+                        { text: 'in' },
+                        { text: 'the' },
+                        { text: 'world' },
+                    ]}
+                    cursorClassName="bg-red-500"
+                />
+            </WavyBackground>
         </main>
     );
 }
