a1ae1ddbee77c38d89b0ec4f374bf3715e036934
Arnab Chakraborty <iam-arnab@outlook.com>
2025-02-27 17:08:14 +0530

    Refactor configuration files and components for consistency; add Prettier configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,26 @@
-import { FloatingNav } from "@/components/ui/floating-navbar";
-import { ModeToggle } from "@/components/ui/mode-toggle";
-import { Home as HomeIcon, CircleHelp } from "lucide-react";
+import { FloatingNav } from '@/components/ui/floating-navbar'
+import { ModeToggle } from '@/components/ui/mode-toggle'
+import { Home as HomeIcon, CircleHelp } from 'lucide-react'
 
 export default function Home() {
-  const navItems = [
-    {
-      name: "Home",
-      link: "/",
-      icon: <HomeIcon />,
-    },
-    {
-      name: "About",
-      link: "/about",
-      icon: <CircleHelp />,
-    },
-  ];
-  return (
-    <main className="relative h-screen w-screen overflow-hidden">
-      <div className="absolute right-4 top-4 z-10 flex flex-col gap-2">
-        <ModeToggle />
-      </div>
-      <FloatingNav navItems={navItems} />
-    </main>
-  );
+    const navItems = [
+        {
+            name: 'Home',
+            link: '/',
+            icon: <HomeIcon />,
+        },
+        {
+            name: 'About',
+            link: '/about',
+            icon: <CircleHelp />,
+        },
+    ]
+    return (
+        <main className="relative h-screen w-screen overflow-hidden">
+            <div className="absolute top-4 right-4 z-10 flex flex-col gap-2">
+                <ModeToggle />
+            </div>
+            <FloatingNav navItems={navItems} />
+        </main>
+    )
 }
