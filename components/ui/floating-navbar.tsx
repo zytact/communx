d94d1cf4ecd7c1d8a6c0975904f6903c76e0d45a
Arnab Chakraborty <iam-arnab@outlook.com>
2025-02-27 17:25:44 +0530

    Enhance floating navigation component styling for improved layout and spacing

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -31,7 +31,7 @@ export const FloatingNav = ({
                     duration: 0.2,
                 }}
                 className={cn(
-                    'fixed inset-x-0 top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-8 py-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black',
+                    'fixed inset-x-0 top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-10 rounded-full border border-transparent bg-white px-16 py-4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black',
                     className
                 )}
             >
@@ -40,11 +40,13 @@ export const FloatingNav = ({
                         key={`link=${idx}`}
                         href={navItem.link}
                         className={cn(
-                            'relative flex items-center space-x-1 text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300'
+                            'relative flex items-center space-x-2 text-neutral-600 hover:text-neutral-500 dark:text-neutral-50 dark:hover:text-neutral-300'
                         )}
                     >
-                        <span className="block sm:hidden">{navItem.icon}</span>
-                        <span className="hidden text-sm sm:block">
+                        <span className="block text-lg sm:hidden">
+                            {navItem.icon}
+                        </span>
+                        <span className="hidden text-base font-medium sm:block">
                             {navItem.name}
                         </span>
                     </Link>
